77c4620f1071b3c6e040c94615c612d9f7936724
BreakDown-CS <chanon.sungchoom@gmail.com>
2026-05-18 17:04:09 +0700

    setup DockerFile

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { LoginForm } from "./auth.schema";
 
 export const AuthLogin = async (data: LoginForm) => {
     try {
-        const response = await api.post("http://127.0.0.1:4001/auth/login", data);
+        const response = await api.post("http://127.0.0.1/setting/auth/login", data);
         
         return response
     } catch (error) {
