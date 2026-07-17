9508d5dd5119c1dc0cfb84759d1d64623f784229
BreakDown-CS <chanon.sungchoom@gmail.com>
2026-07-17 12:51:04 +0700

    แก้ไข config ชั่วคราว

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,3 +1,9 @@
-const apiSetting = process.env.NEXT_PUBLIC_API_SETTING_URL || "http://localhost";
+const apiSetting =
+    process.env.NEXT_PUBLIC_API_SETTING_URL || "http://localhost";
+
+console.log("================================");
+console.log("NEXT_PUBLIC_API_SETTING_URL =", process.env.NEXT_PUBLIC_API_SETTING_URL);
+console.log("apiSetting =", apiSetting);
+console.log("================================");
 
 export default apiSetting;
